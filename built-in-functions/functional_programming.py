d18eecea779901f9ff0913b4bb10f5d1294e68b7
iitsmel <iitsmel@users.noreply.github.com>
2025-07-10 00:13:11 +0800

    feat. built in functions of functional programming (lambda)

--- built-in-functions/functional_programming.py
+++ built-in-functions/functional_programming.py
@@ -3,18 +3,12 @@
 
 numbers = [1, 2, 3, 4, 5]
 
+sum_numbers = sum(numbers)
+lambda_example = lambda a: a + 10
 mapped = list(map(lambda x: x * 2, numbers))
 filtered = list(filter(lambda x: x % 2 == 0, numbers))
-reduced_sum = sum(numbers)
 
-def isOdd(x):
-    return x % 2 != 0
-
-filtered_odds = list(filter(isOdd, numbers))
-mapped_square = list(map(lambda x: x**2, numbers))
-
+print(f"sum                   : {sum_numbers}")
+print(f"lamba                 : {lambda_example(1)}")
 print(f"map (double)          : {mapped}")
 print(f"filter (even numbers) : {filtered}")
-print(f"sum (reduce)          : {reduced_sum}")
-print(f"filter (odd numbers)  : {filtered_odds}")
-print(f"map (square)          : {mapped_square}")
